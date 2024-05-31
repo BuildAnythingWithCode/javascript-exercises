86786c1419eca357d5afa18b17c27b0d2e30cc92
BuildAnythingWithCode <168317593+BuildAnythingWithCode@users.noreply.github.com>
2024-05-31 21:30:24 +0800

    This one was much easier for me

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+  let reverse = '';
+    for (i = text.length - 1; i >= 0; i--) {
+      reverse = reverse + text[i];
+    }
+  return reverse;
 };
 
 // Do not edit below this line
