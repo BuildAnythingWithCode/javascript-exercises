8f547b40b7c97b9d305cefdd2100debabe8c7716
BuildAnythingWithCode <168317593+BuildAnythingWithCode@users.noreply.github.com>
2024-06-01 21:36:05 +0800

    Did this one with very little help

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,8 @@ const sumAll = function(num1, num2) {
   let finalSum = 0;
   if (num1 < 0 || num2 < 0) {
     return 'ERROR';
+  } else if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return 'ERROR';
   } else if (num1 < num2) {
     for (let i = num1; i <= num2; i++) {
     finalSum = finalSum + i;
@@ -12,6 +14,8 @@ const sumAll = function(num1, num2) {
     finalSum = finalSum + i;
   }
   return finalSum;
+  } else {
+    return 'ERROR';
   }
 };
 
