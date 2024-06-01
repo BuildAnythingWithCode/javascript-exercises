74f5a087e2aa2ff96faa815dfe8857418423c7d1
BuildAnythingWithCode <168317593+BuildAnythingWithCode@users.noreply.github.com>
2024-06-01 19:49:11 +0800

    Pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeItems) {
+  let newArray = [];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] !== removeItems) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
