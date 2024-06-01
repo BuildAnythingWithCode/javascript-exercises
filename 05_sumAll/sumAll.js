351120e10890ce103e730aef42325ebff5fe4ec7
BuildAnythingWithCode <168317593+BuildAnythingWithCode@users.noreply.github.com>
2024-06-01 21:27:04 +0800

    Almost there...

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,18 @@
 const sumAll = function(num1, num2) {
   let finalSum = 0;
-  for (let i = num1; i <= num2; i++) {
+  if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  } else if (num1 < num2) {
+    for (let i = num1; i <= num2; i++) {
     finalSum = finalSum + i;
   }
   return finalSum;
+  } else if (num1 > num2) {
+    for (let i = num2; i <= num1; i++) {
+    finalSum = finalSum + i;
+  }
+  return finalSum;
+  }
 };
 
 // Do not edit below this line
